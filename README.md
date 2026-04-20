82fbd5417d8394f8f1c4c82163b8c4f097ecf72e
tomas <theraptor44@hotmail.com>
2026-04-20 20:16:46 -0300

    reset implementado, esqueleto de estadisticas y logica de conteo

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dice-simulator
 Proyecto web de simulación de tiradas de dados del grupo 20 de programación III UTN FRBB.
+
+Consigna:
+# Ejercicio 20: Simulador de dados con estadísticas
+
+### Construye una interfaz con controles y resultados usando **Flexbox**. Al presionar un botón, **JavaScript** debe simular el lanzamiento de dos dados y mostrar la suma obtenida. Lleva además un conteo de cuántas veces apareció cada suma posible durante la sesión.
--- js/script.js
+++ js/script.js
@@ -1,20 +1,39 @@
+// HISTORIAL INICIALIZADO VACIO
 const timeline = [];
 
+// CONTADOR INICIALIZADO EN 0
+let counter = 0
+
+// LOGICA DE TIRADA
 function roll() {
   const dice1 = Math.ceil(Math.random() * 6);
   const dice2 = Math.ceil(Math.random() * 6);
   const sum = dice1 + dice2;
+  counter = counter + 1;
   timeline.push([dice1, dice2]);
   document.getElementById("first-dice").src = `assets/img/dice-${dice1}.svg`;
   document.getElementById("second-dice").src = `assets/img/dice-${dice2}.svg`;
   document.getElementById("sum").textContent = `${sum}`;
+  document.getElementById("counter").textContent = counter;
   renderTimeline();
 }
 
+// RENDERIZADO DE HISTORIAL DE TIRADAS
 function renderTimeline() {
   const last10 = timeline.slice(-10);
   const ul = document.getElementById("timeline");
   ul.innerHTML = last10
     .map((pair) => `<li>${pair[0]}<small>&</small>${pair[1]}</li>`)
     .join("")
+}
+
+// FUNCION RESET A ESTADO INICIAL
+function reset() {
+  timeline.length = 0;
+  counter = 0;
+  document.getElementById("sum").textContent = "0";
+  document.getElementById("first-dice").src = "assets/img/dice-1.svg";
+  document.getElementById("second-dice").src = "assets/img/dice-1.svg";
+  document.getElementById("counter").textContent = "0"
+  renderTimeline();
 }
