783364a5f864428ceb4a4faccfd176db325a560c
naimneman99 <neman99@proton.me>
2026-04-22 02:21:59 -0300

    style: add dice animations

--- js/script.js
+++ js/script.js
@@ -1,13 +1,38 @@
 const timeline = [];
 function roll() {
+  // Randomizamos valores para los dados
   const dice1 = Math.ceil(Math.random() * 6);
   const dice2 = Math.ceil(Math.random() * 6);
   const sum = dice1 + dice2;
+
+  // Lanzar dados en escalera
+  animateDiceRoll("first-dice", `assets/img/dice-${dice1}.svg`);
+  setTimeout(() => {
+    animateDiceRoll("second-dice", `assets/img/dice-${dice2}.svg`);
+  }, 200);
+  setTimeout(() => {
+    animateDiceRoll("sum", `${sum}`);
+  }, 500);
+
+  // Actualizar y renderizar timeline
   timeline.push([dice1, dice2]);
-  document.getElementById("first-dice").src = `assets/img/dice-${dice1}.svg`;
-  document.getElementById("second-dice").src = `assets/img/dice-${dice2}.svg`;
-  document.getElementById("sum").textContent = `${sum}`;
   renderTimeline();
+}
+
+// Animation handler
+function animateDiceRoll(id, newAsset) {
+  const dice = document.getElementById(id);
+  // Triggers roll animation
+  dice.classList.add("roll");
+  // Transitions from roll to reveal
+  setTimeout(() => {
+    dice.classList.remove("roll");
+    if (newAsset !== undefined) dice.src = newAsset;
+    if (id === "sum") { dice.textContent = newAsset };
+    // Triggers reveal animation
+    dice.classList.add("reveal");
+    setTimeout(() => dice.classList.remove("reveal"), 500);
+  }, 600);
 }
 
 function renderTimeline() {
